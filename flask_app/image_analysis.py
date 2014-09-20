4e1824ef3a04ef2a863ea6db58ac487b4841d924
Nikhil Buduma <nbuduma@fitbit.com>
2014-09-20 11:44:30 -0400

    static and image analysis update

--- flask_app/image_analysis.py
+++ flask_app/image_analysis.py
@@ -1,14 +1,36 @@
 import urllib
 import requests
+import os
 
 def upload(filename):
-    with open("path/to/file.png", "rb") as f:
-        data = f.read()
-        image_data = {
-            "api_key": "MlNqKOB0gvIJ9dBz",
-            "api_secret": "CWf5gIEQqoo59fRy",
-            "jobs": "object_add",
-            "base64": encoded
-        }
-        resp = requests.post('https://rekognition.com/func/api/', params=image_data)
-        return resp
+    encoded = urllib.quote(open(filename, "rb").read().encode("base64"))
+    params = {
+        "api_key": "MlNqKOB0gvIJ9dBz",
+        "api_secret": "CWf5gIEQqoo59fRy",
+        "jobs": "object_add",
+        "base64": encoded,
+        "name_space" : "hackathon_testing",
+        "user_id": "test",
+    }
+    resp = requests.request("POST","https://rekognition.com/func/api/index.php", data=params)
+    print resp.status_code
+    print resp.content
+    return resp
+
+def from_cwd(relative_path):
+    return os.path.join(os.getcwd(), relative_path.lstrip('\/'))
+
+def analyze(filename):
+    encoded = urllib.quote(open(filename, "rb").read().encode("base64"))
+    params = {
+        "api_key": "MlNqKOB0gvIJ9dBz",
+        "api_secret": "CWf5gIEQqoo59fRy",
+        "jobs": "scene_understanding_3",
+        "base64": encoded,
+    }
+    resp = requests.request("POST","https://rekognition.com/func/api/index.php", data=params)
+    print resp.status_code
+    print resp.content
+    return resp
+
+analyze("static/pikachu.jpg")
