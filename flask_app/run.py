80e229813723affd8db6819788762c598a16a90e
Anish Athalye <me@anishathalye.com>
2014-09-20 09:31:03 -0400

    Add queueing

--- flask_app/run.py
+++ flask_app/run.py
@@ -1,6 +1,14 @@
 import os
 from flask import Flask, request, redirect, url_for, jsonify
 from werkzeug import secure_filename
+import pika
+import atexit
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+                   'localhost'))
+atexit.register(lambda: connection.close())
+channel = connection.channel()
+channel.queue_declare('files_queue', durable = True)
 
 UPLOAD_FOLDER = os.getcwd() + '/../run/data'
 
@@ -13,6 +21,12 @@ def upload_file():
         if file:
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            channel.basic_publish(exchange='',
+                    routing_key="files_queue",
+                    body=filename,
+                    properties=pika.BasicProperties(
+                        delivery_mode = 2, # make message persistent
+                    ))
             return '%s\n' % jsonify({'result': 'success'})
 
 if __name__ == '__main__':
