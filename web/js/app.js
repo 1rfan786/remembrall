ffac2bf8cdd0dce5da44f2ffbae1c15ad6583677
Shelby Pefley <spefley@mit.edu>
2014-09-20 10:00:43 -0400

    Start building services for receiving data

--- web/js/app.js
+++ web/js/app.js
@@ -1,11 +1,20 @@
 var app = angular.module('rememberall', []);
 
-app.controller('MainPanelCtrl', function($scope) {
-  $scope.value = 0;
+app.controller('MainPanelCtrl', function($scope, testDataFactory) {
+  $scope.savedLib = testDataFactory.savedLib;
 });
 
 app.controller('SideBarCtrl', function($scope) {
   $scope.feeling = 'happy';
+});
+
+app.factory('testDataFactory', function() {
+  return { 
+    savedLib: [
+      {name: 'entry1', frame: '../media/photo.JPG', video: 'clip.mp4'},
+      {name: 'entry2', frame: '../media/photo2.JPG', video: 'clip2.mp4'},
+    ]
+  };
 });
 
 
