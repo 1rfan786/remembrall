5119a86992ecf4e9a7a3a2f56b7ed935e68a26e5
Nikhil Buduma <nbuduma@fitbit.com>
2014-09-20 03:22:40 -0400

    HTTP file upload endpoints

---
+++ flask_app/run.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask, request, redirect, url_for, jsonify
+from werkzeug import secure_filename
+
+UPLOAD_FOLDER = os.getcwd() + "/data"
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+        file = request.files['file']
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return jsonify({'result': "success"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
